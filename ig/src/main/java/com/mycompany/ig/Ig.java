ca73ec4d63cd36522aa1f5aff4fc9d2928727423
pablo6387 <56855366+pablo6387@users.noreply.github.com>
2023-08-20 19:31:32 -0300

    Initial commit

---
+++ ig/src/main/java/com/mycompany/ig/Ig.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ig;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Ig {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
